8a1b80769f1ac958194f13a85e04d17985825f1a
meronash <mahirarona@gmail.com>
2026-09-09 17:34:09 +0700

    feat: add competition experience category

--- main/models.py
+++ main/models.py
@@ -9,6 +9,7 @@ class Experience(models.Model):
         ('part-time', 'Part-Time'),
         ('full-time', 'Full-Time'),
         ('freelance', 'Freelance'),
+        ('competition', 'Competition')
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
